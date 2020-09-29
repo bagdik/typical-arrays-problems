46e0f2069355220903cf3331b116236e10eb44d5
Bahdan Pryimachuk <bahdanchik@tut.by>
2020-09-29 14:11:24 +0300

    feat: Implement functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return arguments[0] !== undefined && array.length ? array.reduce((min, item) => min < item ? min : item) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return arguments[0] !== undefined && array.length ? array.reduce((max, item) => max > item ? max : item) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return arguments[0] !== undefined && array.length ? array.reduce((sum, item) => sum + item, 0) / array.length : 0;
 }
